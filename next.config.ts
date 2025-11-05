1989f610fcad9b9fb6fcad161c7bd6685efd22e3
Web Development Intern <64804433+USC-Development-Intern@users.noreply.github.com>
2025-11-05 12:29:50 -0500

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [{ source: "/", destination: "/ucc-map-2d", permanent: false }];
-  },
-};
-export default nextConfig;
